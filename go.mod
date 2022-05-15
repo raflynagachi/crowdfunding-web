ea58253215ddf7040e5da77d08fff62165d11631
raflynagachi <rafly.rigannagachi@gmail.com>
2022-05-15 15:28:41 +0700

    add CLI tools in main

--- go.mod
+++ go.mod
@@ -10,6 +10,9 @@ require (
 
 require (
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.4 // indirect
+	github.com/spf13/cobra v1.4.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 )
--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"flag"
+
+	"github.com/raflynagachi/crowdfunding-web/app/cmd"
 	"github.com/raflynagachi/crowdfunding-web/app/config"
 )
 
 func main() {
 	dbConfig, _ := config.ConfigEnv()
-
 	config.OpenDB(dbConfig)
+
+	flag.Parse()
+	narg := flag.NArg()
+	if narg != 0 {
+		cmd.RootCmd.Execute()
+	}
+
+	// userRepository := repositories.NewUserRepository(db)
+	// userRepository.Create(models.User{
+	// 	Name: "Nagachi",
+	// })
+
 }
